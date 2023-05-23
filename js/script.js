2d7f4660b2939cd00e04dedae7c8088c01b67220
Володимир Расказов (Volodymyr Raskazov) <wladras77@gmail.com>
2023-05-23 22:08:28 +0300

    some changes

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
 "use strict"
+document.addEventListener('DOMContentLoaded', () => {
+	// инициализация слайдера
+	new ItcSimpleSlider('.slider', {
+		loop: true,
+		autoplay: false,
+		interval: 5000,
+		swipe: true,
+	});
+});
